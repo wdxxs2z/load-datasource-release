aa3bae6a703353027e980442e2b9fa82a933fa98
wdxxs2z <243219650@qq.com>
2018-01-03 17:23:05 -0800

    add src

---
+++ src/load-datasource/src/main/java/com/wdxxs2z/loaddatasource/LoadDatasourceApplication.java
@@ -0,0 +1,12 @@
+package com.wdxxs2z.loaddatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadDatasourceApplication.class, args);
+	}
+}
